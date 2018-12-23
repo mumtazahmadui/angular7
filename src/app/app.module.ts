95929285a6a7c7c7e523a6de1d2eb03270ee0725
Mumtaz <Mumtaz.Ahmed@ihsmarkit.com>
2018-12-23 20:12:12 +0530

    first-step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
+import { TempComponent } from './temp/temp/temp.component';
+
+const routes: Routes = [
+  { path: '', component: RentalComponent },
+  { path: 'temp', component: TempComponent }
+  // { path: '', redirectTo: '/rentals', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RentalComponent,
+    RentalListComponent,
+    RentalListItemComponent,
+    TempComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    BrowserModule
+    //AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
